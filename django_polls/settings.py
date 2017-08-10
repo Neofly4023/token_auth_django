d20d927152d1a6cd7ab627ea13c70f33c85f51bc
Remache Amine Riad <ea_remache@esi.dz>
2017-08-10 23:01:56 +0100

    Update settings.py
    Updated the settings.py file

--- django_polls/settings.py
+++ django_polls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0pns*xjbdv9k+ak*%88m0d3%rat4=ln@_u3qt_ox2f0k=jr3%9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost' , '127.0.0.1']
 
 
 # Application definition
